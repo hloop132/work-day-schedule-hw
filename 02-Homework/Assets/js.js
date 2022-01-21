5efb51cd6fa9f46ce130e3c31b0d6b198c55824d
heather loop <hloop132@gmail.com>
2022-01-21 15:02:24 -0800

    planner

--- 02-Homework/Assets/js.js
+++ 02-Homework/Assets/js.js
@@ -1,37 +1,53 @@
 //moment add attr of text area to present, past, future
-// moment().format("MMM Do YY"); 
+// moment().format("MMM Do YY");
 var currentDay = moment().format("MMM Do YYYY");
-$('#currentDay').text(currentDay);
+$("#currentDay").text(currentDay);
+console.log(currentDay);
+var currentHour = moment().format("HH");
 
-//current hour < past
-//current hour === present
-//current hour> future 
-function currentHourEl(){
-    var currentHour = moment().format('HH');
-    $('.hour').each(function (){ var hourSection = parseInt($(this).attr('id'))
-//run each text area el throw function 
-if (hourSection < currentHour) {
-    $(this).siblings('.description').addClass("past")
-} 
-else if (hourSection == currentHour) { 
-    $(this).siblings(".description").addClass("present")
-} 
-else {
-    $(this).siblings(".description").addClass("future")
-}
+var textAreaEl = $("textarea");
+console.log(textAreaEl);
 
-currentHourEl()
-})}
+$.each(textAreaEl, function () {
+  var dataTime = $(this).data("time");
+  console.log();
 
-    //local storage
-    //function save
-    //function load
-    $(document).ready(function (){
-        $(".saveBtn").on("click", function(){
-            var value = $(this).siblings(".description").val()
-            var time = $(this).siblings().attr("id")
-      
-            localStorage.setItem("key", value)
-            } )})
+  if (currentHour == dataTime) {
+    $(this).addClass("present");
+  } else if (currentHour > dataTime) {
+    $(this).addClass("past");
+  } else {
+    $(this).addClass("future");
+  }
+});
+$("button").click(function () {
+  var dataTime = $(this).data("time");
+  for (let i = 0; i < textAreaEl.length; i++) {
+    var item = $(textAreaEl[i]).data("time");
+    if (dataTime == item) {
+      console.log($(textAreaEl[i]).val());
+      localStorage.setItem(dataTime, $(textAreaEl[i]).val() );
+    }
+
+    //localStorage.setItem('settings', JSON.stringify(settings));
+
+    //let storedSettings = JSON.parse(localStorage.getItem('settings'));
+    //console.log(storedSettings);
     
-            
+
+    var words = JSON.parse(localStorage.getItem("text"));
+
+    $("#9-am-text .data-time").val(localStorage.getItem("9"));
+    $("#10-am-text .description").val(localStorage.getItem("10"));
+    $("#11-am-text .text-area").val(localStorage.getItem("11"));
+    $("#12-pm-text .data-time").val(localStorage.getItem("12"));
+    $("#1-pm-text .data-time").val(localStorage.getItem("13"));
+    $("#2-pm-text .data-time").val(localStorage.getItem("14"));
+    $("#3-pm-text .data-time").val(localStorage.getItem("15"));
+    $("#4-pm-text .data-time").val(localStorage.getItem("16"));
+    $("#5-pm-text .data-time").val(localStorage.getItem("17"));
+  }
+
+});
+
+//load any saved data from LocalStorage - do this for each hour created. Should follow html 24 hour to 12 hour conversion.
